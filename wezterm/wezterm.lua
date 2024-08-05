c98adec0c9442fe489c8c01b8e269ca79f34152a
kungfun-rumblefish <kanstantsin.nerushkin@rumblefish.pl>
2024-08-05 15:24:33 +0200

    allign font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local action = wezterm.action
 local config = {
     window_decorations = "RESIZE",
     enable_tab_bar = true,
-    font_size = 16,
+    font_size = 12,
     font = fonts.get_font(),
     color_scheme = "catppuccin-mocha",
     window_padding = {
@@ -26,15 +26,15 @@ local config = {
     },
     initial_cols = 200,
     initial_rows = 40,
-    background = {{
+    background = { {
         source = {
-            File = "/Users/kungfun/Documents/darknoise.jpg"
+            File = wezterm.config_dir .. "/background/darknoise.jpg"
         },
         hsb = {
             brightness = 1,
         },
         opacity = 0.85,
-    }},
+    } },
     keys = {
         keys.cmd_key(
             "R",
@@ -47,7 +47,7 @@ local config = {
 }
 
 wezterm.on("gui-startup", function()
-    local tab, pane, window = mux.spawn_window{}
+    local tab, pane, window = mux.spawn_window {}
     window:gui_window():maximize()
 end)
 
