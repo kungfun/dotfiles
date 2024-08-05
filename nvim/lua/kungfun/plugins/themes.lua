e64518ff9d39b0a2f461f4ead892839acce147ec
Kostya Nerushkin <kungfun@Kostyas-MacBook-Pro.local>
2024-08-05 19:04:25 +0200

    fix tint

--- nvim/lua/kungfun/plugins/themes.lua
+++ nvim/lua/kungfun/plugins/themes.lua
@@ -1,59 +1,63 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        config = function()
-            local theme = require("tokyonight")
+	{
+		"folke/tokyonight.nvim",
+		config = function()
+			local theme = require("tokyonight")
 
-            theme.setup({
-                style = "night",
-                transparent = true,
-            })
-        end,
-    },
-    { "projekt0n/github-nvim-theme" },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                flavor = "mocha",
-                dim_inactive = {
-                    enabled = true,
-                    shade = "dark",
-                    percentage = 0.95,
-                },
-            })
-        end,
-    },
-    {
-        --colors = cyberdream,
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        config = function()
-            require("ibl").setup()
-        end,
-    },
-    "levouh/tint.nvim",
-    {
-        "folke/zen-mode.nvim",
-        opts = {
-            window = {
-                width = .5,
-                height = 1,
-                backdrop = 1,
-            },
-            plugins = {
-                options = {
-                    enabled = true,
-                    laststatus = 3,
-                }
-            },
-            wezterm = {
-                enabled = true,
-            }
-        }
-    },
-    -- TODO: learn how to use
-    "stevearc/dressing.nvim",
+			theme.setup({
+				style = "night",
+				transparent = true,
+			})
+		end,
+	},
+	{ "projekt0n/github-nvim-theme" },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavor = "mocha",
+				dim_inactive = {
+					enabled = true,
+					shade = "dark",
+					percentage = 0.95,
+				},
+			})
+		end,
+	},
+	{
+		--colors = cyberdream,
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			require("ibl").setup()
+		end,
+	},
+	{
+		"folke/zen-mode.nvim",
+		opts = {
+			window = {
+				width = 0.5,
+				height = 1,
+			},
+			plugins = {
+				options = {
+					enabled = true,
+					laststatus = 3,
+				},
+			},
+			wezterm = {
+				enabled = true,
+			},
+		},
+	},
+	{
+		"levouh/tint.nvim",
+		config = function()
+			require("tint").setup()
+		end,
+	},
+	-- TODO: learn how to use
+	"stevearc/dressing.nvim",
 }
