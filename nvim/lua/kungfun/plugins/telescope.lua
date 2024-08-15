f8056417b0aafeae26d451b66f49f904b30b3a2d
kungfun-rumblefish <kanstantsin.nerushkin@rumblefish.pl>
2024-08-15 15:47:33 +0200

    adjust borders

--- nvim/lua/kungfun/plugins/telescope.lua
+++ nvim/lua/kungfun/plugins/telescope.lua
@@ -1,76 +1,74 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-			"folke/todo-comments.nvim",
-			"debugloop/telescope-undo.nvim",
-			{
-				"nvim-telescope/telescope-live-grep-args.nvim",
-				version = "^1.0.0",
-			},
-		},
-		config = function()
-			local telescope = require("telescope")
-			local actions = require("telescope.actions")
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+            "folke/todo-comments.nvim",
+            "debugloop/telescope-undo.nvim",
+            {
+                "nvim-telescope/telescope-live-grep-args.nvim",
+                version = "^1.0.0",
+            },
+        },
+        config = function()
+            local telescope = require("telescope")
+            local actions = require("telescope.actions")
 
-			telescope.setup({
-				defaults = {
-					path_display = { "smart" },
-					layout_config = {
-						horizontal = {
-							preview_width = 0.75,
-							results_width = 0.2,
-							preview_cutoff = 1,
-						},
-						width = 0.87,
-						height = 0.80,
-					},
-					mappings = {
-						i = {
-							["<C-k>"] = actions.move_selection_previous,
-							["<C-j>"] = actions.move_selection_next,
-							["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-							["<C-_"] = actions.which_key,
-						},
-						n = {
-							["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-						},
-					},
-				},
-				extensions = {
-					fzf = {
-						fuzzy = true,
-						override_generic_sorter = true,
-						override_file_sorter = true,
-						case_mode = "smart_case",
-					},
-					undo = {
-						use_delta = true,
-						use_custom_command = nil,
-						side_by_side = false,
-						vim_diff_opts = {
-							ctxlen = vim.o.scrolloff,
-						},
-						entry_format = "state #$ID, $STAT, $TIME",
-						mappings = {
-							i = {
-								["<C-cr>"] = require("telescope-undo.actions").yank_additions,
-								["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
-								["<cr>"] = require("telescope-undo.actions").restore,
-							},
-						},
-					},
-				},
-			})
+            telescope.setup({
+                defaults = {
+                    path_display = { "smart" },
+                    layout_config = {
+                        horizontal = {
+                            preview_width = 0.75,
+                            results_width = 0.2,
+                            preview_cutoff = 1,
+                        },
+                    },
+                    mappings = {
+                        i = {
+                            ["<C-k>"] = actions.move_selection_previous,
+                            ["<C-j>"] = actions.move_selection_next,
+                            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                            ["<C-_"] = actions.which_key,
+                        },
+                        n = {
+                            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                        },
+                    },
+                },
+                extensions = {
+                    fzf = {
+                        fuzzy = true,
+                        override_generic_sorter = true,
+                        override_file_sorter = true,
+                        case_mode = "smart_case",
+                    },
+                    undo = {
+                        use_delta = true,
+                        use_custom_command = nil,
+                        side_by_side = false,
+                        vim_diff_opts = {
+                            ctxlen = vim.o.scrolloff,
+                        },
+                        entry_format = "state #$ID, $STAT, $TIME",
+                        mappings = {
+                            i = {
+                                ["<C-cr>"] = require("telescope-undo.actions").yank_additions,
+                                ["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
+                                ["<cr>"] = require("telescope-undo.actions").restore,
+                            },
+                        },
+                    },
+                },
+            })
 
-			telescope.load_extension("live_grep_args")
-			telescope.load_extension("undo")
-			telescope.load_extension("fzf")
+            telescope.load_extension("live_grep_args")
+            telescope.load_extension("undo")
+            telescope.load_extension("fzf")
 
-			vim.cmd([[ autocmd User TelescopePreviewerLoaded setlocal number ]])
-		end,
-	},
+            vim.cmd([[ autocmd User TelescopePreviewerLoaded setlocal number ]])
+        end,
+    },
 }
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,54 +5,53 @@ local mux = wezterm.mux
 local action = wezterm.action
 
 local config = {
-	window_decorations = "RESIZE",
-	enable_tab_bar = true,
-	font_size = 16,
-	font = fonts.get_font(),
-	color_scheme = "catppuccin-mocha",
-	window_close_confirmation = "NeverPrompt",
-	hide_tab_bar_if_only_one_tab = true,
-	window_padding = {
-		left = 40,
-		right = 20,
-		top = 30,
-		bottom = 20,
-	},
-	window_background_opacity = 0.85,
-	use_fancy_tab_bar = false,
-	colors = {
-		tab_bar = {
-			background = "rgba(0,0,0,0.85)",
-		},
-		scrollbar_thumb = "rgba(0,0,0)",
-	},
-	initial_cols = 200,
-	initial_rows = 40,
-	background = {
-		{
-			source = {
-				File = wezterm.config_dir .. "/background/darknoise.jpg",
-			},
-			hsb = {
-				brightness = 1,
-			},
-			opacity = 0.85,
-		},
-	},
-	keys = {
-		keys.cmd_key(
-			"R",
-			action.Multiple({
-				action.SendKey({ key = "\x1b" }),
-				keys.multiple_actions(":w"),
-			})
-		),
-	},
+    window_decorations = "RESIZE",
+    enable_tab_bar = true,
+    font_size = 12,
+    font = fonts.get_font(),
+    color_scheme = "catppuccin-mocha",
+    window_close_confirmation = "NeverPrompt",
+    window_padding = {
+        left = 20,
+        right = 10,
+        top = 15,
+        bottom = 10,
+    },
+    window_background_opacity = 0.95,
+    use_fancy_tab_bar = false,
+    colors = {
+        tab_bar = {
+            background = "rgba(0,0,0,0.95)",
+        },
+        scrollbar_thumb = "rgba(0,0,0)",
+        background = "rgba(1,1,1)"
+    },
+    initial_rows = 40,
+    -- background = {
+    --     {
+    --         source = {
+    --             File = wezterm.config_dir .. "/background/darknoise.jpg",
+    --         },
+    --         hsb = {
+    --             brightness = 0.2,
+    --         },
+    --         opacity = 0.95,
+    --     },
+    -- },
+    keys = {
+        keys.cmd_key(
+            "R",
+            action.Multiple({
+                action.SendKey({ key = "\x1b" }),
+                keys.multiple_actions(":w"),
+            })
+        ),
+    },
 }
 
 wezterm.on("gui-startup", function()
-	local tab, pane, window = mux.spawn_window({})
-	window:gui_window():maximize()
+    local tab, pane, window = mux.spawn_window({})
+    window:gui_window():maximize()
 end)
 
 return config
